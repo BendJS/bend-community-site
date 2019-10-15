1d6dc8f5e3b8589b83bae89d8ed1b6d749cb6dcd
Connor Crossley <ccrossley@cbtnuggets.com>
2019-10-15 07:09:19 -0700

    Resolves #5 testing suite

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Button from '@material-ui/core/Button';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" data-testid="app">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('renders without crashing', () => {
+    const { getByTestId } = render(<App />);
+
+    expect(getByTestId('app')).toMatchSnapshot();
+  });
+
+  it('renders the right link', () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText('Learn React')).toBeVisible();
+  });
 });
