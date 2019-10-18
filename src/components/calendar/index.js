50d84253da2d170e4c007dd0fbfffa25876c2787
markpernotto <mark@pernotto.com>
2019-10-18 00:07:16 -0700

    Calendar Cleanup

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -5,15 +5,33 @@ import fakeEvents from './fakeEvents';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './calendar.css';
 
+const resourceMap = [
+    { resourceId: 1, resourceTitle: 'Board room' },
+    { resourceId: 2, resourceTitle: 'Training room' },
+    { resourceId: 3, resourceTitle: 'Meeting room 1' },
+    { resourceId: 4, resourceTitle: 'Meeting room 2' },
+]
+
 const localizer = momentLocalizer(moment)
 
-const TAOCalendar = props => (
+const selectEvent = (event) => {
+    console.log(`selected event: `, event);
+}
+
+const TAOCalendar = () => (
     <div className="calendar-container">
         <Calendar
             localizer={localizer}
             events={fakeEvents}
             startAccessor="start"
             endAccessor="end"
+            onSelectEvent={selectEvent}
+            resources={resourceMap}
+            resourceIdAccessor="resourceId"
+            resourceTitleAccessor="resourceTitle"
+            eventPropGetter={event => ({
+                className: event.class
+            })}
         />
     </div>
 )
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import './header.css';
 
 function Home() {
   return (
     <header data-testid="app">
       <Link to='/'>Home</Link>
       <Link to='contact-us'>Contact Us</Link>
+        <Link to='calendar'>Calendar</Link>
     </header>
   );
 }
